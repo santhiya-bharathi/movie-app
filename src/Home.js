a4f983661be2e2b774f3f86685f7a5a0747b1854
santhiya-bharathi <santhiyabharathi1999@gmail.com>
2022-02-22 09:57:10 +0530

    color changed

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ export function Home() {
   return (
     <div className="home">
       <h2 className="home-hello">Welcome to movies app</h2>
-      <h6 className="home-det">you can explore movies after signup or login</h6>
+      <h6 className="home-det">you can explore the movies after signup or login</h6>
     </div>
   );
 }
